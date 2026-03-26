62018601c841743323e03ad78007cc0c6c173689
subhamsupriyamohapatra <subhammohapatra633@gmail.com>
2026-03-26 18:27:11 +0530

    portfolio

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     'Software Engineer',
     'FullStack Developer'
   ];
-  // test vercel deploy
+  // test vercel deploy test
   let phraseIndex = 0;
   let charIndex = 0;
   let isDeleting = false;
